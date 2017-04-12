9ddc2bc5b3c428df0de8ba62f36fac184d5b8bb2
elia-maracani <e.maracani@campus.unimib.it>
2017-04-12 15:09:27 +0200

    * added SparseCell to handle cells

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ import com.jmatio.types.MLNumericArray;
 import com.jmatio.types.MLStructure;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by eliam on 12/04/2017.
@@ -14,12 +16,56 @@ public class Main {
         MatFileReader reader = new MatFileReader("poisson2D.mat");
         Map<String, MLArray> matrix = reader.getContent();
 
-        for (String key : matrix.keySet()) {
-            MLStructure prova = (MLStructure) matrix.get(key);
-            MLNumericArray numericArray = (MLNumericArray) prova.getField("A");
+        MLStructure prova = (MLStructure) matrix.get("Problem");
+        MLNumericArray numericArray = (MLNumericArray) prova.getField("A");
 
-            System.out.println(prova.getField("A").contentToString());
-            System.out.println(numericArray.get(116, 0));
+        for (int i=0; i<numericArray.getM(); i++) {
+            for (int j = 0; i < numericArray.getN(); j++) {
+                double value = numericArray.get(i, j).doubleValue();
+
+                if (value != 0) {
+
+                }
+            }
+        }
+
+        //System.out.println(mat);
+    }
+
+
+    public class SparseCell {
+        private int rowIndex;
+        private int colIndex;
+        private double value;
+
+        public SparseCell(int row, int col, double value) {
+            setColIndex(col);
+            setRowIndex(row);
+            setValue(value);
+        }
+
+        public int getRowIndex() {
+            return rowIndex;
+        }
+
+        public void setRowIndex(int rowIndex) {
+            this.rowIndex = rowIndex;
+        }
+
+        public int getColIndex() {
+            return colIndex;
+        }
+
+        public void setColIndex(int colIndex) {
+            this.colIndex = colIndex;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        public void setValue(double value) {
+            this.value = value;
         }
     }
 }
