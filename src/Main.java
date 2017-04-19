246996d32f29eecbce80c78bd7d585d17d600b99
elia-maracani <e.maracani@campus.unimib.it>
2017-04-19 21:15:09 +0200

    * introduced console logging to see what happens * other things I don't know how to explain here

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,90 @@
 import com.jmatio.io.MatFileReader;
 import com.jmatio.types.MLArray;
-import com.jmatio.types.MLNumericArray;
+import com.jmatio.types.MLSparse;
 import com.jmatio.types.MLStructure;
 
-import org.la4j.matrix.sparse.CCSMatrix;
 import org.la4j.matrix.sparse.CRSMatrix;
 import org.la4j.operation.MatrixVectorOperation;
+import org.la4j.operation.VectorVectorOperation;
 import org.la4j.operation.ooplace.OoPlaceMatrixByVectorMultiplication;
+import org.la4j.operation.ooplace.OoPlaceVectorsSubtraction;
 import org.la4j.vector.DenseVector;
-import org.la4j.vector.dense.BasicVector;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.Map;
 
 /**
- * Created by eliam on 12/04/2017.
+ *
+ * Created by Elia Maracani on 12/04/2017.
  */
 public class Main {
+
+    private static final String FILE_NAME = "poisson3Db.mat";
+    private static Date start = new Date(), now = new Date();
+
     public static void main(String[] args) throws IOException {
-        MatFileReader reader = new MatFileReader("poisson3Db.mat");
+        printLog("Program started.");
+        printLog("Loading file " + FILE_NAME + " in memory ...");
+        MatFileReader reader = new MatFileReader(FILE_NAME);
+        printLog("Completed");
+
+        printLog("Retrieving matrix 'A' ...");
         Map<String, MLArray> matlabMatrix = reader.getContent();
-
         MLStructure structure = (MLStructure) matlabMatrix.get("Problem");
-        MLNumericArray bigA = (MLNumericArray) structure.getField("A");
+        MLSparse bigA = (MLSparse) structure.getField("A");
+        printLog("Completed. Found matrix " + bigA.getM() + "x" + bigA.getN() + ".");
 
-        CRSMatrix matrix = new CRSMatrix(bigA.getM(), bigA.getN());
-        for (int i=0; i<bigA.getM(); i++) {
-            for (int j = 0; j<bigA.getN(); j++) {
-                double value = bigA.get(i, j).doubleValue();
+        printLog("Creating empty matrix 'A' ...");
+        CRSMatrix matrix = CRSMatrix.zero(bigA.getN(), bigA.getM());
+        printLog("Completed.");
 
-                if (value != 0)
-                    matrix.set(i, j, value);
-            }
+        printLog("Retrieving non-zero values indexes ...");
+        int rowsIndex[] = bigA.getIR();
+        int colsIndex[] = bigA.getIC();
+        printLog("Completed. There are " + rowsIndex.length + " values.");
+        printLog("Setting matrix 'A' values based on non-zero values found ...");
+        for (int i=0; i<rowsIndex.length; i++) {
+            double val = bigA.get(rowsIndex[i], colsIndex[i]);
+
+            matrix.set(rowsIndex[i], colsIndex[i], val);
         }
+        printLog("Completed.");
 
-        double x [] = new double[bigA.getM()];
-        for (int i=0; i<x.length; i++)
-            x[i] = 1;
-        DenseVector vector = new BasicVector(x);
+        printLog("Computing A*x ...");
+        MatrixVectorOperation matrixVectorMultiply = new OoPlaceMatrixByVectorMultiplication();
+        DenseVector x = DenseVector.constant(bigA.getM(), 1.0);
+        DenseVector smallB = (DenseVector) matrix.multiply(x);
+        printLog("Completed.");
 
-        MatrixVectorOperation test = new OoPlaceMatrixByVectorMultiplication();
-        DenseVector smallB = (DenseVector) test.apply(matrix, vector);
+        printLog("Computing A*b ...");
+        DenseVector Xe = (DenseVector) matrixVectorMultiply.apply(matrix, smallB);
+        printLog("Completed.");
 
-        printSmallB(smallB);
+        printLog("Computing x-Xe ...");
+        VectorVectorOperation vectorVectorSub = new OoPlaceVectorsSubtraction();
+        DenseVector ris = (DenseVector) vectorVectorSub.apply(x, Xe);
+        printLog("Completed.");
+
+        printLog("Computing ||x-Xe|| - ||Xe|| ...");
+        double error = ris.norm() / Xe.norm();
+        printLog("Completed");
+        printLog("!!!! Error --> " + error + " <-- !!!!");
     }
 
-    static private void printBigA(CCSMatrix matrix, int rows, int cols) {
-        for (int i=0; i<rows; i++) {
-            for (int j = 0; j<cols; j++) {
-                double value = matrix.get(j, i);
+    static private void printLog(String message) {
+        String msg = (((new Date()).getTime() - start.getTime()) / 1000) + "s # " + message;
 
-                if (value != 0)
-                    System.out.println((j + 1) + " - " + (i + 1) + " - " + value);
-            }
+        System.out.println(msg);
+    }
+
+    static private void printBigA(CRSMatrix matrix, int[] rowsIndex, int[] colsindex) {
+        for (int i=0; i<rowsIndex.length; i++) {
+            int row = rowsIndex[i];
+            int col = colsindex[i];
+
+            if (matrix.nonZeroAt(row, col))
+                System.out.println("--- ROW:" + row + " COL:" + col + " VAL:" + matrix.get(row, col));
         }
     }
 
